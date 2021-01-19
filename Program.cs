0cf954d7af40a7290d886ccbaf1527e2548164ee
Remco HvA <r.van.swieten@hva.nl>
2021-01-19 20:19:24 +0100

    Converts Centipede

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
-namespace BaseProject
+namespace Centipede
 {
     public static class Program
     {
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Centipede())
                 game.Run();
         }
     }
