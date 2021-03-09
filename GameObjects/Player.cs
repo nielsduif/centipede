06e22ae07311766f23c7a0d467e783e629f5594b
nielsduif <n.duivenn2@hva.nl>
2021-03-09 12:21:52 +0100

    player

---
+++ GameObjects/Player.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Centipede.GameObjects
+{
+    class Player : SpriteGameObject
+    {
+        public Player() : base("spr_player")
+        {
+            Mouse.SetPosition(235, 500);
+            Origin = new Vector2(sprite.Width * .5f, sprite.Height * .5f);
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            position = inputHelper.MousePosition;
+            base.HandleInput(inputHelper);
+        }
+    }
+}
--- GameStates/PlayingState.cs
+++ GameStates/PlayingState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Centipede.GameObjects;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,12 @@ namespace Centipede.Content.GameStates
 {
     class PlayingState : GameObjectList
     {
+        Player player;
         public PlayingState()
         {
             Add(new SpriteGameObject("spr_background"));
+            player = new Player();
+            Add(player);
         }
     }
 }
